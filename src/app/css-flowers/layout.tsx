2c1044ce02844b5618370eee095a86fe9a92478a
devaryakjha <dev.jha.arya@gmail.com>
2023-11-14 17:10:39 +0530

    Add metadata for CSS Flowers page

--- src/app/css-flowers/layout.tsx
+++ src/app/css-flowers/layout.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next/types";
 import "./flowers.scss";
+
+export const metadata: Metadata = {
+  title: "CSS Flowers",
+  description: "CSS Flowers",
+};
 
 export default function RootLayout({
   children,
